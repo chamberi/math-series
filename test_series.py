88b1e3e3b810bffecc83f238d4909d95b78a6e97
Colin Lamont <colinlamont@gmail.com>
2016-12-06 13:39:31 -0800

    added test_series and first hard-coded test

---
+++ test_series.py
@@ -0,0 +1,8 @@
+"""This is the testing file for fibonacci and lucas mather functions."""
+
+
+def test1(n):
+    """Test fibonacci for 0 == 0."""
+    from series import fibonacci
+    if (n == 0):
+        assert fibonacci(n) == 0
