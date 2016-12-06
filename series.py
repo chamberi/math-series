bb2e3f7b5aa5feb222cccd898eab8e4423e5dac6
Colin Lamont <colinlamont@gmail.com>
2016-12-06 14:36:43 -0800

    built out lucas function against test scenarios

--- series.py
+++ series.py
@@ -9,3 +9,13 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def lucas(n):
+    """Return the nth value in the lucas series."""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
