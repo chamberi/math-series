9d2fceeb55b2c24661b2b7498d5f41423388912e
Maelle Vance <maellevance@gmail.com>
2016-12-06 14:00:36 -0800

    created series module defined the fibonacci function and wrote it so it would pass the first test. will need to be refactored

---
+++ series.py
@@ -0,0 +1,7 @@
+"""This module defines functions that implement mathematical series."""
+
+
+def fibonacci(n):
+        """Return the nth value in fibonacci series."""
+        if n == 0:
+            return 0
--- test_series.py
+++ test_series.py
@@ -1,8 +1,8 @@
 """This is the testing file for fibonacci and lucas mather functions."""
 
 
-def test1(n):
+def test1():
     """Test fibonacci for 0 == 0."""
     from series import fibonacci
-    if (n == 0):
-        assert fibonacci(n) == 0
+    # if n == 0:
+    assert fibonacci(0) == 0
