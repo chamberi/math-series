09d6f2f11c5c4fc78386fd32cafa2272a9ba458f
Maelle Vance <maellevance@gmail.com>
2016-12-06 14:24:52 -0800

    added an else statement to fibonacci function which will now work outside of edge cases, solved recursively

--- series.py
+++ series.py
@@ -2,8 +2,10 @@
 
 
 def fibonacci(n):
-        """Return the nth value in fibonacci series."""
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
+    """Return the nth value in fibonacci series."""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
