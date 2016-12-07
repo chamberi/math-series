10cb734a619adc919ec1099765630b42ab5a0fc6
Colin Lamont <colinlamont@gmail.com>
2016-12-06 21:21:45 -0800

    added a lucas iteration function, and added the iterations to the if name main section

--- series.py
+++ series.py
@@ -38,14 +38,24 @@ def lucas(n):
 
 def iter_lucas(n):
     """Return the nth value in the lucas series using iteration."""
-# same model than the iter_fibonacci function
+    a = 2
+    b = 1
+    if n == 1:
+        return a
+    elif n == 2:
+        return b
+    else:
+        for num in range(n - 2):
+            b = b + a
+            a = b - a
+        return b
 
 
 def sum_series(x, y=0, z=1):
     """Return the result of sum_series given x, optional arguments y, z."""
-    if x == 0:
+    if x == 1:
         return y
-    elif x == 1:
+    elif x == 2:
         return z
     else:
         return sum_series((x - 1), y, z) + sum_series((x - 2), y, z)
@@ -56,14 +66,24 @@ if __name__ == "__main__":
     This module defines functions that implement mathematical series.
 
     fibonacci(n):
-    Returns the nth value in the fibonacci series
+    Returns the nth value in the fibonacci series using recursion.
     >>> fibonnaci(2)
     1
+
+    iter-fibonacci(n):
+    Returns the nth value in the fibonacci series using iteration.
+    >>> fibonnaci(4)
+    2
 
     lucas(n):
     return the nth value in the fibonacci series
     >>> lucas(2)
     3
+
+    iter_lucas(n):
+    return the nth value in the fibonacci series
+    >>> lucas(4)
+    4
 
     sum_series(x, y=0, z=1):
     return the xth value in this series based on the optional arguments y and z
