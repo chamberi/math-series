6e1ca41f16d2c7672d1cbb7fc4c20b2bc1127cc5
Maelle Vance <maellevance@gmail.com>
2016-12-06 20:13:29 -0800

    altered lucas function so it returns the nth value and not value of n. Tests pass

--- series.py
+++ series.py
@@ -28,9 +28,9 @@ def iter_fibonacci(n):
 
 def lucas(n):
     """Return the nth value in the lucas series."""
-    if n == 0:
+    if n == 1:
         return 2
-    elif n == 1:
+    elif n == 2:
         return 1
     else:
         return lucas(n - 1) + lucas(n - 2)
